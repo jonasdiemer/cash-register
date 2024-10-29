0d4bd10aae1910aea1e6859dc8ac59a69e313302
Jonas Diemer <jonasdiemer@gmail.com>
2024-10-29 17:28:54 +0100

    fixing deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [sveltekit()],
 
-  // Add this section for better logging
   logLevel: "info",
   clearScreen: false,
+  // Add this to help resolve the node-conditions issue
+  resolve: {
+    conditions: ["browser"],
+  },
 });
